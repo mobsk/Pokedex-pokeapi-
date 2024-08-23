60f4365f79bd61c1aef22d2666e735794dd3dd0a
mobsk <erick.brayan2007@gmail.com>
2024-08-23 10:55:03 -0300

    barra de busca

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ document.addEventListener('DOMContentLoaded',function() {
             console.error('Erro ao buscar dados de Pokémon', error);
         });
 });
+async function searchPokemon() {
+    const name = document.getElementById('pokemon-name').value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Pokémon não encontrado');
+        const data = await response.json();
+
+        // Exibir informações do Pokémon
+        const pokemonInfo = document.getElementById('pokemon-info');
+        pokemonInfo.innerHTML = `
+            <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            <p>Altura: ${data.height / 10} m</p>
+            <p>Peso: ${data.weight / 10} kg</p>
+            <p>Tipo(s): ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+        `;
+    } catch (error) {
+        const pokemonInfo = document.getElementById('pokemon-info');
+        pokemonInfo.innerHTML = `<p>${error.message}</p>`;
+    }
+}
